d686971b1436a1d5a027a71095cbff9c0f2c50a7
emma <eqbrown@isf.edu.hk>
2023-08-07 17:06:05 +0200

    fix allowed sites

--- permatopia_site/settings.py
+++ permatopia_site/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_hib^w!pns-=ea+l3k0e=nv##_9mj)_ij0&bgs5x13k(r4=s7+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://simple-farm-tracking-5b0965f19d6e.herokuapp.com/','127.0.0.1:8000/']
+ALLOWED_HOSTS = ['https://simple-farm-tracking-5b0965f19d6e.herokuapp.com/','*']
 
 
 # Application definition
